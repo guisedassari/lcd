095cedd653609e2aa75d79c91911be5b818f1485
guilherme <guilherme@localhost>
2016-09-01 13:19:04 -0300

    melhorias

--- application/views/painel/galerias/index.php
+++ application/views/painel/galerias/index.php
@@ -1,6 +1,6 @@
 <div class="row">
     <div class="col-lg-12">
-        <h1 class="page-header">Galeria Corporativo</h1>
+        <h1 class="page-header">Galeria de imagens</h1>
     </div>
 </div>
 <!-- /.row -->
--- application/views/painel/menus/cabecalho.php
+++ application/views/painel/menus/cabecalho.php
@@ -38,7 +38,6 @@ if (!($_SESSION['usuario_logado'])) {
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="index.html">SB Admin v2.0</a>
                 </div>
                 <!-- /.navbar-header -->
                 <ul class="nav navbar-top-links navbar-right">
@@ -57,21 +56,9 @@ if (!($_SESSION['usuario_logado'])) {
                 <div class="navbar-default sidebar" role="navigation">
                     <div class="sidebar-nav navbar-collapse">
                         <ul class="nav" id="side-menu">
-                            <li class="sidebar-search">
-                                <div class="input-group custom-search-form">
-                                    <input type="text" class="form-control" placeholder="Search...">
-                                    <span class="input-group-btn">
-                                        <button class="btn btn-default" type="button">
-                                            <i class="fa fa-search"></i>
-                                        </button>
-                                    </span>
-                                </div>
-                                <!-- /input-group -->
+                            <li class="sidebar-search text-center">
+                                <?= img('assets/site/img/logo.png', FALSE, array('class' => 'logo-login')); ?>
                             </li>
-                            <li>
-                                <a href="#"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
-                            </li>
-
                             <li>
                                 <?= anchor('welcome/index', '<i class="fa fa-picture-o" aria-hidden="true"></i> Galeria', array('class' => '')); ?>
                             </li>
